a301165f3534f6ee0e070f2708f226600dd103f7
ragulprabakaran <ragulprabakaran@gmail.com>
2017-06-08 09:58:40 +0530

    Add files via upload

---
+++ alphabets.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Alphabets
+{
+    
+ public static void main(String []args)
+ {
+        
+  char ch;
+       
+  Scanner in=new Scanner(System.in);
+     
+  System.out.println("Enter the character");
+     
+  ch=in.next().charAt(0);
+      
+  if(ch>='A'&&ch<='Z'||ch>='a'&&ch<='z')
+    
+  {
+        
+   System.out.println("The given character is an alphabet");  
+  
+  }
+     
+  else
+      
+   System.out.println("The given character is not an alphabet");  
+        
+
+ }
+
+}
